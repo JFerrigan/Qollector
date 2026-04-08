77d1376223ae54d44da056c2cd1399acf2957a7d
Jdf <jakedavidferrigan@gmail.com>
2026-04-07 22:45:15 -0500

    Initial Commit

---
+++ VinylCollector/VinylCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylCollectorApp.swift
+//  VinylCollector
+//
+//  Created by Jake Ferrigan on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct VinylCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VinylCollectorTests/VinylCollectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  VinylCollectorTests.swift
+//  VinylCollectorTests
+//
+//  Created by Jake Ferrigan on 4/7/26.
+//
+
+import Testing
+@testable import VinylCollector
+
+struct VinylCollectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
